0f3a31dfa33996ff040964a56b8355f1b81cc1e7
Arafat-Rahaman <arafatfbook@gmail.com>
2026-08-26 00:32:36 +0600

    updated

--- views/login.php
+++ views/login.php
@@ -65,6 +65,7 @@ $errors = $_SESSION['errors'] ?? [];
                     <input name="password" type="password" placeholder="Password">
                     <span class="error-message" id="password-error"><?php echo $errors['password'] ?? ''; ?></span>
                 </div>
+                
 
                 <div class="remember-forgot">
                     <!-- <label>
